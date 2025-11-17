937a2b244aaafa502c5051713d753ad071b06f98
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-11-17 20:39:58 +0530

    Changes in routing for single page portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path : 'about', component: AboutComponent}
+  {path : '', component: AppComponent},
+  {path : '**', redirectTo: ''}
 ];
 
 @NgModule({
