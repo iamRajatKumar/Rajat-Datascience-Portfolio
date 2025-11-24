8e8ae48bef79740b77446fba50ca90c8d6b62ee9
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-11-24 19:06:09 +0530

    project component ts added

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+ projects = [
+  {
+    title: 'AI Stock Market Predictor',
+    desc: 'A Machine Learning model that predicts stock movements using historical trends and sentiment analysis.',
+    img: 'assets/images/project2.png',
+    demo: '#',
+    github: '#'
+  },
+  {
+    title: 'Quiz App (Microservices Architecture)',
+    desc: 'An end-to-end quiz platform built using Spring Boot microservices, Eureka Registry, and Angular frontend.',
+    img: 'assets/images/project1.png',
+    demo: '#',
+    github: '#'
+  },
+  {
+    title: 'Data Science Portfolio',
+    desc: 'A collection of data science projects built with Python, ML, and statistical analysis.',
+    img: 'assets/images/project1.png',
+    demo: '#',
+    github: '#'
+  },
+   {
+    title: 'AI Stock Market Predictor',
+    desc: 'A Machine Learning model that predicts stock movements using historical trends and sentiment analysis.',
+    img: 'assets/images/project2.png',
+    demo: '#',
+    github: '#'
+  },
+  {
+    title: 'Quiz App (Microservices Architecture)',
+    desc: 'An end-to-end quiz platform built using Spring Boot microservices, Eureka Registry, and Angular frontend.',
+    img: 'assets/images/project1.png',
+    demo: '#',
+    github: '#'
+  },
+  {
+    title: 'Data Science Portfolio',
+    desc: 'A collection of data science projects built with Python, ML, and statistical analysis.',
+    img: 'assets/images/project1.png',
+    demo: '#',
+    github: '#'
+  }
+];
 
 }
